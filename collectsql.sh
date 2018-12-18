434d0fb750e047bd3d07084d78e2088d64c72969
马昭 <mazhao@iMac-2.local>
2018-12-18 18:57:11 +0800

    update

---
+++ collectsql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source_folder=$1
+target_folder=$2
+
+echo "# 数据库初始化sql" > $target_folder
+
+for element in `ls $source_folder`
+do  
+    dir_or_file=$1"/"$element
+    if [ -d $dir_or_file ]
+    then 
+        getdir $dir_or_file
+    else
+        echo $dir_or_file" >> $target_folder"
+        cat $dir_or_file >> $target_folder
+    fi  
+done
