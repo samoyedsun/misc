7f8e3a9abe57d7519648b34ce29fa6057be1ab06
马昭 <mazhao@iMac-2.local>
2018-12-20 19:09:30 +0800

    update

---
+++ onekeykubeadm.sh
@@ -0,0 +1,29 @@
+# 加入kubernetes套件yum仓库
+cat <<EOF > kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+sudo mv kubernetes.repo /etc/yum.repos.d/
+rm kubernetes.repo
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sudo sed -i 's/^SELINUX=disable$/SELINUX=permissive/' /etc/selinux/config
+
+# 查找kubectl kubelet kubeadm
+sudo yum search kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+# 安装kubectl kubelet kubeadm
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+# 设置kubelet服务开启自启动，并启动它(然后它会每隔几秒重启一次等待kubeadm init告诉它做什么)
+sudo systemctl enable kubelet && sudo systemctl start kubelet
+
+# 查看kubelet状态
+sudo systemctl status kubelet
