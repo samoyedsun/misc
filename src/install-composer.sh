8574c8bdfead92a4845052d0c5908c62992dcb40
mz <mz@mzdeMacBook-Pro.local>
2019-01-18 16:19:17 +0800

    update

---
+++ src/install-composer.sh
@@ -0,0 +1,3 @@
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
