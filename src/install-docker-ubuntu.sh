cd546866401268e1bb2c464d0a37a4f9f0930ee8
mz <mz@mzdeMacBook-Pro.local>
2019-03-03 20:56:57 +0800

    update

--- src/install-docker-ubuntu.sh
+++ src/install-docker-ubuntu.sh
@@ -10,15 +10,13 @@ apt remove docker docker-engine docker.io -y
 apt update -y
 
 # 安装软件包,以允许apt通过https使用镜像仓库
-apt install \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     software-properties-common \
-     -y
+apt install apt-transport-https -y
+apt install ca-certificates -y
+apt install curl -y
+apt install software-properties-common -y
 
 # 添加docker官方的GPG密钥
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 # 验证指纹是否为 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88
 apt-key fingerprint 0EBFCD88
