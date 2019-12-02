1b2ea9453be23caaf645a856ab37e698beb8915e
mz <mz@ddpkcc.com>
2019-12-02 11:09:27 +0800

    update

---
+++ src/startzentao.sh
@@ -0,0 +1,7 @@
+docker run -d -p 80:80 -p 3306:3306 \
+        -e ADMINER_USER="root" -e ADMINER_PASSWD="password" \
+        -e BIND_ADDRESS="false" \
+        -v ${HOME}/zentaodata/:/opt/zbox/ \
+        --add-host smtp.exmail.qq.com:163.177.90.125 \
+        --name zentao-server \
+        idoop/zentao:latest
