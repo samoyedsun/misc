90ebe2d917b76d21d7b4a1d3ac35b97fc44ebeaa
JohnMa <JohnMa@huanying.com>
2020-10-26 15:10:05 +0800

    update

--- src/startzentao.sh
+++ src/startzentao.sh
@@ -1,7 +1,6 @@
 docker run -d -p 9980:80 -p 3306:3306 \
-        -e ADMINER_USER="root" -e ADMINER_PASSWD="password" \
-        -e BIND_ADDRESS="false" \
-        -v ${HOME}/zentaodata/:/opt/zbox/ \
-        --add-host smtp.exmail.qq.com:163.177.90.125 \
-        --name zentao-server \
-        idoop/zentao:12.3
+	-e MYSQL_ROOT_PASSWORD='123456' \
+	-v ${HOME}/zentaopms:/www/zentaopms \
+	-v ${HOME}/mysqldata:/var/lib/mysql \
+	--name zentao-server \
+	easysoft/zentao:12.3.3
