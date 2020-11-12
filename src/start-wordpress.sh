e5247befdeb861ae39fc21694703b4304eb51b3d
JohnMa <JohnMa@huanying.com>
2020-11-12 13:13:52 +0800

    update

--- src/start-wordpress.sh
+++ src/start-wordpress.sh
@@ -1,2 +1,2 @@
 docker run -d --privileged=true --name videoms -v ${HOME}/data/mysql:/var/lib/mysql -e MYSQL_DATABASE=wordpress -e MYSQL_ROOT_PASSWORD=233333  mysql
-docker run -d --name videowp -e WORDPRESS_DB_HOST=mysql -e WORDPRESS_DB_PASSWORD=233333 -p 80:80 --link videoms:mysql wordpress
+docker run -d --name videowp -e WORDPRESS_DB_HOST=mysql -v ${HOME}/html:/var/www/html -e WORDPRESS_DB_PASSWORD=233333 -p 80:80 --link videoms:mysql wordpress
