2d02923a2197c833091e06f75377336e4001f750
JohnMa <samoyedsun@gmail.com>
2021-03-13 18:02:43 +0800

    update

--- src/start-mongo.sh
+++ src/start-mongo.sh
@@ -1,5 +1,5 @@
 docker run -it -d -p 27017:27017 \
-    -v ${HOME}/data/mongo:/root/data \
+    -v ${HOME}/data/mongo:/data/db \
     -e MONGO_INITDB_ROOT_USERNAME=bcwallet \
     -e MONGO_INITDB_ROOT_PASSWORD=2habYaVFQFKmuji5 \
     --name mongo mongo
