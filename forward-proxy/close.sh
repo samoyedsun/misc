a86b7b68190b58f4a3d632b1c4631dba91e2311a
John Ma <samoyedsun@gmail.com>
2022-05-04 16:17:09 +0800

    add nginx-ftp proxy

--- forward-proxy/close.sh
+++ forward-proxy/close.sh
@@ -1,2 +1,4 @@
 docker stop forward-proxy
 docker rm forward-proxy
+docker stop forward-ftp-proxy
+docker rm forward-ftp-proxy
--- forward-proxy/start.sh
+++ forward-proxy/start.sh
@@ -3,3 +3,9 @@ docker run -it -d \
     -v ${PWD}/nginx.conf:/usr/local/nginx/conf/nginx.conf \
     --name forward-proxy \
     reiz/nginx_proxy
+docker run -it -d \
+    -p 15135:15135 \
+    -p 15129:15129 \
+    -v ${PWD}/nginx-ftp.conf:/etc/nginx/nginx.conf \
+    --name forward-ftp-proxy \
+    tsuru/nginx-tsuru
